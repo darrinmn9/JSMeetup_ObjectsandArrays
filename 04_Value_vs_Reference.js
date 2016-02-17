4513924aa6dd15ad031745579bf97d346b1c199b
darrinmn9 <dnagengast9@gmail.com>
2016-02-16 17:00:37 -0800

    added another val vs ref example

--- 04_Value_vs_Reference.js
+++ 04_Value_vs_Reference.js
@@ -21,3 +21,25 @@ myObject.x = 5;
 
 console.log(myOtherObject.x); // --> undefined
 console.log(anotherObject.x); // --> 5
+
+
+
+/* ------------------------------------------- */
+
+var button = {
+  count: 0,
+  click: function(){
+    console.log('button has been clicked!');
+    this.count++;
+  }
+};
+
+
+var buttonCount = button.count;
+console.log(buttonCount);
+
+button.click();
+button.click();
+
+console.log(buttonCount);   // ??
+console.log(button.count);  // ??
